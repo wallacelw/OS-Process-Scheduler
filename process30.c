706296569a630e7d329f40ad5f85e7f15d134e79
wallace <wallacebtlw@gmail.com>
2024-07-22 09:10:41 -0300

    created 15 and 30 processes

---
+++ process30.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+
+int main() {
+    
+    // get start time in seconds
+    double start_time = clock();
+    start_time /= CLOCKS_PER_SEC; 
+
+    // simulate a real process
+    long int i = 0;
+    long limit = 14e9;
+    for(i=0;i<limit;i++) {}
+
+    // get end time in seconds
+    double end_time = clock();
+    end_time /= CLOCKS_PER_SEC;
+
+    // compute runtime and print it
+    double run_time = end_time - start_time;
+    printf("Time Elapsed: %.6lf \n", run_time);
+}
